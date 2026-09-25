ab4787d22586a7366e774985c560d1b8782e289b
Apoorva Saini <apoorvasaini55@gmail.com>
2026-09-25 09:52:31 +0530

    Ques3

--- lab3/Ques3.cpp
+++ lab3/Ques3.cpp
@@ -23,18 +23,11 @@ public:
         return a * b;
     }
 };
-
 int main() {
     Calculator c;
-
-    // Inline function
     cout << "Square of 5 = " << c.square(5) << endl;
-
-    // Default argument
     cout << "Add with two values = " << c.add(10, 20) << endl;
     cout << "Add with one value = " << c.add(10) << endl;
-
-    // Function overloading
     cout << "Integer multiplication = "
          << c.multiply(5, 4) << endl;
 
